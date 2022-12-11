976d7293ade04705720e0325b454d4b3006368fe
JScodeKiddie <JScodeKiddie@gmail.com>
2022-12-11 21:13:28 +0100

    fixed the input fields

--- app.js
+++ app.js
@@ -1,93 +1,22 @@
-const result = document.getElementById('result');
-const buttonsWrapper = document.querySelector('.wrapper');
+import Calculator from './src/calculator.js';
 
-const calculator = {
-  firstNumber: '',
-  secondNumber: '',
-  current: null,
-  operator: null,
-};
+const result = document.getElementById("result")
+const calculator = new Calculator();
 
-let operatorIsClicked = false;
-let equalsIsClicked = false;
-
-buttonsWrapper.addEventListener('click', ev => {
-  if (ev.target.nodeName !== 'BUTTON') {
-    return;
-  }
-
+document.querySelector('.wrapper').addEventListener('click', ev => {
   const { target } = ev;
-
-  const actions = {
-    equals: calculate,
-    clears: clearResult,
-  };
-
-  actions[target.value] ? actions[target.value]() : generateCalc(target);
-});
-
-const generateCalc = target => {
-  calculator.current = target.value;
-  const { current } = calculator;
-
-  if (!isNaN(current)) {
-    if (equalsIsClicked && !operatorIsClicked) {
-      calculator.firstNumber = '';
-    }
-    if (operatorIsClicked) {
-      calculator.secondNumber += current;
-      updateResult(calculator.secondNumber);
-    } else {
-      equalsIsClicked = false;
-      calculator.firstNumber += current;
-      updateResult(calculator.firstNumber);
-    }
-  } else {
-    operatorIsClicked = true;
-    calculator.operator = current;
-  }
-};
-
-const calculate = () => {
-  const { firstNumber, secondNumber, operator } = calculator;
-  let currentResult;
-
-  if (!firstNumber || !secondNumber || !operator) {
+  if (target.nodeName !== 'BUTTON') {
     return;
   }
-  switch (operator) {
-    case '*':
-      currentResult = +firstNumber * +secondNumber;
-      break;
-    case '/':
-      currentResult = +firstNumber / +secondNumber;
-      break;
-    case '-':
-      currentResult = +firstNumber - +secondNumber;
-      break;
-    case '+':
-      currentResult = +firstNumber + +secondNumber;
-      break;
+  const { value } = target;
+
+  if (!isNaN(value)) {
+    result.value += value
+    calculator.current += value;
+  } else {
+    calculator.calculate();
+    calculator.operator = value;
   }
 
-  calculator.firstNumber = currentResult;
-  calculator.secondNumber = '';
-  calculator.operator = null;
-  equalsIsClicked = true;
-  operatorIsClicked = false;
-  return (result.value = currentResult == undefined ? '' : currentResult);
-};
-
-const clearResult = () => {
-  calculator.firstNumber = '';
-  calculator.secondNumber = '';
-  calculator.current = null;
-  calculator.operator = null;
-  operatorIsClicked = false;
-  equalsIsClicked = false;
-  result.value = '';
-};
-
-const updateResult = value => {
-  result.value = value;
-};
+  console.log(calculator);
+});
---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+class Calculator {
+  constructor(previous, current) {
+    this.previousTextElement = previous;
+    this.currentTextElement = current;
+  }
+}
+
+export default Calculator;
